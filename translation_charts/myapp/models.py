1f8e68d17727d77defb1ead1d5e7a2a0462fb43f
Marius <121916282+S-Marius@users.noreply.github.com>
2023-01-16 23:32:07 +0100

    Update models.py

--- translation_charts/myapp/models.py
+++ translation_charts/myapp/models.py
@@ -5,9 +5,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    translations_done = models.IntegerField(default=0, null=True)
-    date = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    translations_done = models.IntegerField(default=0, null=True, blank=True)
+    date = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.translations_done} done by {self.user.username} at {self.date}"
